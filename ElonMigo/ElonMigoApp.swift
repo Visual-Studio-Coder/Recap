3031b943837dfeea1d7c43e0b5527bec18fae4cf
Vaibhav Satishkumar <vaibhavguy39@gmail.com>
2024-05-25 08:59:05 -0700

    Inital commit

---
+++ ElonMigo/ElonMigoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecapApp.swift
+//  Recap
+//
+
+//
+
+import SwiftUI
+
+@main
+struct RecapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
