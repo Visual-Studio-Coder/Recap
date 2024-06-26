73a58ad26517c8d6d7e4579188f5762961b068eb
Vaibhav Satishkumar <vaibhavguy39@gmail.com>
2024-06-25 22:34:26 -0700

    UI Changes/Improvements + AI free response answer grading

--- ElonMigo/ElonMigoApp.swift
+++ ElonMigo/ElonMigoApp.swift
@@ -18,10 +18,12 @@ struct RecapApp: App {
     }
     
     @StateObject private var quizStorage = QuizStorage()
+    @StateObject private var userPreferences = UserPreferences()
     
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(userPreferences)
                 .environmentObject(quizStorage)
                 .splashView {
                     ZStack {
